c37becd0ded198315de56e422ab38834a965cd8b
Joshua Howland <development@jkhowland.com>
2014-06-11 13:46:25 -0600

    Initial Commit

---
+++ SendField/SFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SFAppDelegate.h
+//  SendField
+//
+//  Created by Joshua Howland on 6/11/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SendField/SFViewController.h
@@ -0,0 +1,13 @@
+//
+//  SFViewController.h
+//  SendField
+//
+//  Created by Joshua Howland on 6/11/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFViewController : UIViewController
+
+@end
